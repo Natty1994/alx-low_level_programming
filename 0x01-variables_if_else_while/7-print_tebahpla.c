b36fc25b9a4a2c0b3824a58a37a0a59106c7a03a
natty <natty.araya.g@gmail.com>
2023-06-16 05:53:46 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * RETURN: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'z';
+
+
+	if (; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
