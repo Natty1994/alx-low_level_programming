1e74b936aeea5de4e22f3576401722e469d0daa7
natty <natty.araya.g@gmail.com>
2023-07-03 19:23:08 -0700

    mandatory tasks

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+{
+	int i, j, f;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		f = 1; /*flag status*/
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				f = 0; /*success*/
+				break;
+			}
+			j++;
+		}
+		if (f == 1)
+			break;
+		i++;
+	}
+
+	return (i);
+}
